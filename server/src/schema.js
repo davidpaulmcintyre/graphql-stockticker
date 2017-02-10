4abf2c8ebef1e831bf73fae88df7ad4e07fb820d
Mike Isman <mikeisman@gmail.com>
2017-02-10 18:53:42 -0500

    Stock type

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,94 +4,44 @@ import {
   GraphQLString,
   GraphQLList,
 } from 'graphql';
-import {authors, books} from './db';
-import authorType from './authorType';
-import {bookType, bookInputType} from './bookType';
+import {stocks} from './db';
+import stockType from './stockType';
 
 const rootFields = {
-  authors: {
-    type: new GraphQLList(authorType),
+  stocks: {
+    type: new GraphQLList(stockType),
     resolve: _ => {
-      return authors;
+      return stocks;
     }
   },
-  books: {
-    type: new GraphQLList(bookType),
-    resolve: _ => {
-      // Resolve functions can return promises
-      return Promise.resolve(books);
-    }
-  },
-  bookByID: {
-    type: bookType,
+  stockByID: {
+    type: stockType,
     args: {
       id: {
         type: GraphQLString,
       }
     },
     resolve: (object, {id}, context, info) => {
-      return books.find(book => `book-${book.id}` == id);
+      return stocks.find(stock => `stock-${stock.id}` == id);
     }
   },
-  bookSearch: {
-    type: new GraphQLList(bookType),
+  stockSearch: {
+    type: new GraphQLList(stockType),
     args: {
       keyword: {
         type: GraphQLString,
       }
     },
     resolve: (object, {keyword}, context, info) => {
-      return books.filter(book => book.title.includes(keyword));
+      return stocks.filter(stock => stock.companyName.includes(keyword) || stock.symbol == keyword);
     }
   },
-  secret: {
-    type: GraphQLString,
-    resolve: (object, args, context, {rootValue}) => {
-      const user = rootValue.user;
-      if(!user) {
-        return 'only authorized users can know the secret';
-      }
-      if(user.name === 'admin' && user.pass === '123') {
-        return 'howdy admin';
-      }
-      return 'who are you?';
-    }
-  }
 };
-
-// Single "viewer" object for Relay root query compatibility
-const Viewer = new GraphQLObjectType({
-  name: 'Viewer',
-  fields: rootFields,
-});
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'QueryRoot',
-    fields: {
-      viewer: {
-        type: Viewer,
-        resolve: () => ({}),
-      },
-      ...rootFields,
-    }
-  }),
-  mutation: new GraphQLObjectType({
-    name: 'MutationRoot',
-    fields: {
-      addBook: {
-        type: bookType,
-        args: {
-          book: {
-            type: bookInputType,
-          }
-        },
-        resolve: (object, {book}) => {
-          books.push(book);
-          return book;
-        }
-      }
-    }
+    fields: rootFields,
   }),
 });
 
