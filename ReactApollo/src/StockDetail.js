854912f118d0fbc5f51965370ffdfe1a4b369623
davidmcintyre <davidpmcintyre@yahoo.com>
2017-02-10 21:52:12 -0500

    stock details

--- ReactApollo/src/StockDetail.js
+++ ReactApollo/src/StockDetail.js
@@ -5,7 +5,19 @@ import { graphql } from 'react-apollo';
 
 const StockDetail = (props) => {
     console.log(props);
-    return <div>Stock detail...</div>;
+    return (
+      <div>
+        <h2>Stock detail</h2>
+        <div><b>CompanyName: <span>props.companyName</span>  </div>
+        <div><b>Symbol: <span>props.symbol</span>  </div>
+        <div><b>Price: <span>props.price</span>  </div>
+        <div><b>Change: <span>props.change</span>  </div>
+        <div><b>Change Percentage: <span>props.changePct</span>  </div>
+        <div><b>Open Price: <span>props.openPrice</span>  </div>
+        <div><b>Market Cap: <span>props.marketCap</span>  </div>
+        <div><b>Exchange: <span>props.exchange</span>  </div>
+      </div>
+    );
 };
 
 export default StockDetail;
