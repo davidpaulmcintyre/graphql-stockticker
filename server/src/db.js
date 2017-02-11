570bc33caabc47886c450beef3915b5be13a5272
Mike Isman <mikeisman@gmail.com>
2017-02-10 19:13:44 -0500

    Add openPrice, remove id, start seed data

--- server/src/db.js
+++ server/src/db.js
@@ -1,3 +1,23 @@
 
 export const stocks = [
+  {
+    companyName: 'Facebook',
+    symbol: 'FB',
+    price: 134.19,
+    change: 0.05,
+    changePct: 0.04,
+    openPrice: 134.10,
+    marketCap: '387.81B',
+    exchange: 'NASDAQ',
+  },
+  {
+    companyName: 'Alphabet',
+    symbol: 'GOOG',
+    price: 813.67,
+    change: 4.11,
+    changePct: 0.51,
+    openPrice: 811.70,
+    marketCap: '569.09B',
+    exchange: 'NASDAQ',
+  },
 ];
